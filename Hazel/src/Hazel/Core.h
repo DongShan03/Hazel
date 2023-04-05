9641e3dd02cea6215171722f8ea74750f4acb74c
DongShan03 <1135972990@qq.com>
2023-04-05 15:57:02 +0800

    Logging commit at 15:56 2023/4/5

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -5,7 +5,7 @@
 	#ifdef HZ_BUILD_DLL
 		#define HAZEL_API __declspec(dllexport)
 	#else
-		#define Hazel_API __declspec(dllimport)
+		#define HAZEL_API __declspec(dllimport)
 	#endif // HZ_BUILD_DLL
 #else
 	#error Hazel only support Windows!
