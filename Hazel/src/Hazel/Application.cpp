0550bcda0baf82493fdf76931abacc2702618e0c
DongShan03 <1135972990@qq.com>
2023-04-13 08:55:20 +0800

    Key and Mouse Codes commit at 8:55 2023/4/13

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -47,6 +49,7 @@ namespace Hazel {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
 
 			m_Window->OnUpdate();
 		}
